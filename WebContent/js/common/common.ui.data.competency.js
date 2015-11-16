24d27920137c116801e4bbf42373efc3ea0d99f4
donghyuck <andang.son@gmail.com>
2015-11-16 16:56:41 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

---
+++ WebContent/js/common/common.ui.data.competency.js
@@ -0,0 +1,32 @@
+(function($, undefined) {
+	
+	var extend = $.extend;
+	
+	var Competency = kendo.data.Model.define( {
+	    id: "competencyId", // the identifier of the model
+	    fields: {
+	    	competencyId: { type: "number", editable: true, defaultValue: 0  },    	
+	    	objectType: { type: "number", editable: true, defaultValue: 0  },   
+	    	objectId: { type: "number", editable: true, defaultValue: 0  },   
+	        name: { type: "string", editable: true },
+	        description: { type: "string", editable: true },
+	    },	    
+	    copy : function ( target ){
+	    	target.set("competencyId", this.get("competencyId"));
+	    	
+	    	target.set("objectType", this.get("objectType"));
+	    	target.set("objectId", this.get("objectId"));
+	    	target.set("name", this.get("name"));
+	    	target.set("description", this.get("description"));
+	    	if( typeof this.get("properties") === 'object' )
+	    		target.set("properties", this.get("properties"));
+	    }    
+	});	
+	
+	extend( common.ui.data, {
+		competency:{
+			Competency : Competency		
+		}  
+	} );
+	
+})(jQuery);
