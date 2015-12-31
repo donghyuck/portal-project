3a7e7d35d38b13b37cd1aa6d199f297f7b1543ce
donghyuck <andang.son@gmail.com>
2015-12-31 10:47:58 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

--- WebContent/js/common/common.ui.data.competency.js
+++ WebContent/js/common/common.ui.data.competency.js
@@ -140,7 +140,7 @@
 	    	objectId : { type: "number"},
 			name : { type: "string" },	   
 			description:  { type: "string" },
-			ratingScheme: { type:"object"},
+			ratingScheme: { type:"object", defaultValue: {ratingSchemeId:0}},
 			properties : {type: "object" , defaultValue:"[]"},
 	    	modifiedDate: { type: "date"},
 	        creationDate: { type: "date" }	
