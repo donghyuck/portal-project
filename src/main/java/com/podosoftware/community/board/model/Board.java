0a1ed535b3e11c33913c36bd1670f09e65f6e307
donghyuck <andang.son@gmail.com>
2016-07-28 13:37:43 +0900

    Interface added

---
+++ src/main/java/com/podosoftware/community/board/model/Board.java
@@ -0,0 +1,55 @@
+package com.podosoftware.community.board.model;
+
+import java.sql.Date;
+
+public interface Board {
+
+	public String getBoardCode();
+
+	public void setBoardCode(String boardCode);
+
+	public String getBoardName();
+
+	public void setBoardName(String boardName);
+
+	public long getBoardNo();
+
+	public void setBoardNo(long boardNo);
+
+	public String getWriter();
+
+	public void setWriter(String writer);
+
+	public Date getWriteDate();
+
+	public void setWriteDate(Date writeDate);
+
+	public String getContent();
+
+	public void setContent(String content);
+
+	public String getTitle();
+
+	public void setTitle(String title);
+
+	public String getImage();
+
+	public void setImage(String image);
+
+	public int getReadCount();
+
+	public void setReadCount(int readCount);
+
+	public long getWritingRef();
+
+	public void setWritingRef(long writingRef);
+
+	public long getWritingSeq();
+
+	public void setWritingSeq(long writingSeq);
+
+	public int getWritingLevel();
+
+	public void setWritingLevel(int writingLevel);
+
+}
---
+++ src/main/java/com/podosoftware/community/board/model/QnaBoard.java
@@ -0,0 +1,12 @@
+package com.podosoftware.community.board.model;
+
+public interface QnaBoard extends Board {
+
+	public String getType();
+
+	public void setType(String type);
+
+	public String getCategory();
+
+	public void setCategory(String category);
+}
